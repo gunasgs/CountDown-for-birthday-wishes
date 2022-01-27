bfc59edbfe559cd969775197fed38984a12e91b3
GUNA SGS <96234417+gunasgs@users.noreply.github.com>
2022-01-27 10:04:58 +0530

    Day 21 task

---
+++ Day 21/script.js
@@ -0,0 +1,49 @@
+
+let time = 11;
+setTimeout(() => {
+    document.querySelector(".data").innerHTML = "Let's Go";
+    setTimeout(() => {
+        time--;
+        document.querySelector(".data").innerHTML = time;
+        setTimeout(() => {
+            time--;
+            document.querySelector(".data").innerHTML = time;
+            setTimeout(() => {
+                time--;
+                document.querySelector(".data").innerHTML = time;
+                setTimeout(() => {
+                    time--;
+                    document.querySelector(".data").innerHTML = time;
+                    setTimeout(() => {
+                        time--;
+                        document.querySelector(".data").innerHTML = time;
+                        setTimeout(() => {
+                            time--;
+                            document.querySelector(".data").innerHTML = time;
+                            setTimeout(() => {
+                                time--;
+                                document.querySelector(".data").innerHTML = time;
+                                setTimeout(() => {
+                                    time--;
+                                    document.querySelector(".data").innerHTML = time;
+                                    setTimeout(() => {
+                                        time--;
+                                        document.querySelector(".data").innerHTML = time;
+                                        setTimeout(() => {
+                                            time--;
+                                            document.querySelector(".data").innerHTML = time;
+                                        }, 1000);
+                                        setTimeout(() => {
+                                            document.querySelector(".data").innerHTML = "Happy Republic Day";
+                                        }, 1000);
+                                    }, 1000);
+                                }, 1000);                            
+                              }, 1000);
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);
+            }, 1000);
+        }, 1000);
+    }, 1000);
+    
